90dff64414a536f2923aee0234136b3fc5802f09
Rui-Araujo1409 <rui1409@gmail.com>
2025-10-28 17:06:34 +0000

    final com o erro no npx webpack serve

--- webpack-exercício/src/index.js
+++ webpack-exercício/src/index.js
@@ -1,4 +1,10 @@
 import "./estilos.css";
 import {saudação} from "./saudação";
+import teorba from "./teorba.jpg";
 
+const imagem = document.createElement("img");
+imagem.src = teorba;
+document.body.appendChild(imagem);
+
+console.log(saudação);
 console.log(saudação);
