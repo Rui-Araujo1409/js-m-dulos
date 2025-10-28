a936ac933ff1b449b2291fd0a07e3f41cf1ed8ae
Rui-Araujo1409 <rui1409@gmail.com>
2025-10-28 10:52:23 +0000

    export/import módulos em ES6

---
+++ dois.js
@@ -0,0 +1,10 @@
+//importar um default export e um export nomeado
+import saudação, { textoDespedida } from "./um.js";
+
+
+console.log(saudação);
+console.log(textoDespedida);
+
+
+//importar um elemento default
+//import qualquerCoisa from "./um.js";
